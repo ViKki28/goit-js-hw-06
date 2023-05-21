009b6e71edac76f551035331a38f7fce50d5309a
ViKki28 <bika33903@mail.ru>
2023-05-21 23:45:04 +0300

    Task 1, 2 DONE
    Task 1, 2 DONE

---
+++ js/task-01.js
@@ -0,0 +1,21 @@
+
+const categoriesList = document.querySelector('ul#categories');
+
+
+const categories = categoriesList.querySelectorAll('li.item');
+
+
+console.log('Number of categories:', categories.length);
+
+
+categories.forEach(category => {
+  
+  const title = category.querySelector('h2').textContent;
+
+  
+  const elements = category.querySelectorAll('li');
+
+  
+  console.log('Category:', title);
+  console.log('Elements:', elements.length);
+});
---
+++ js/task-02.js
@@ -0,0 +1,31 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingredientsList = document.querySelector('ul#ingredients');
+
+
+const fragment = document.createDocumentFragment();
+
+
+ingredients.forEach(ingredient => {
+  
+  const listItem = document.createElement('li');
+
+  
+  listItem.textContent = ingredient;
+
+  
+  listItem.classList.add('item');
+
+  
+  fragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(fragment);
+
